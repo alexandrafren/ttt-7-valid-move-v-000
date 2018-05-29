688572b13f0c1196f2cf636464ea7fa7180f7a71
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-29 15:48:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 
 # code your #valid_move? method here
-def valid_move?()
+def valid_move?(board, index)
   if !index.between?(0,8)
     return false
   elsif index.between?(0,8) && !position_taken?
